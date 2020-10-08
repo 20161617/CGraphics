c39149bb1e8ef14394d057767db200822fe1433c
20161617 <70495829+20161617@users.noreply.github.com>
2020-10-09 01:02:07 +0900

    병합 3차

--- Emposter/Assets/yongchan.cs
+++ Emposter/Assets/yongchan.cs
@@ -7,13 +7,16 @@ public class yongchan : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //언제되
-        //냐고
+          
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //언제되
+        //냐고
     }
 }
