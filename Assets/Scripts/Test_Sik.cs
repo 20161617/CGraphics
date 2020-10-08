d911a80bc93bb96eccbe1d7ef38663a35fa64cbb
ChangSik Kim <qlenfrl9914@gmail.com>
2020-10-08 21:38:43 +0900

    깃허브 연동(김창식)
    김창식 깃허브 연동 테스트

---
+++ Assets/Scripts/Test_Sik.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//20161626 김창식 깃허브 테스트
+public class Test_Sik : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
