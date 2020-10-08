d108c14fcdcb261ebf09693a5b4d71108e78baf3
LeeNamGwan <70495598+LeeNamGwan@users.noreply.github.com>
2020-10-09 00:59:16 +0900

    실험1
    실험1

--- Emposter/Assets/yongchan.cs
+++ Emposter/Assets/yongchan.cs
@@ -7,7 +7,7 @@ public class yongchan : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //실험 1
     }
 
     // Update is called once per frame
